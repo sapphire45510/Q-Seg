02b964a806d3f73d2663d7423b4bceba8b7bb1ef
sapphire <sapphire45510@gmail.com>
2026-07-02 15:53:53 +0800

    原本用真實硬體，現在改Simulated Annealing

--- qseg/dwave_utils.py
+++ qseg/dwave_utils.py
@@ -1,80 +1,67 @@
 import time
 import dimod
-from dwave.system.samplers import DWaveSampler
-from dwave.system.composites import EmbeddingComposite
 import networkx as nx
 import numpy as np
 
-def dwave_solver(linear, quadratic, private_token, runs=10000, **kwargs):
-  """
-  Solve a binary quadratic model using D-Wave sampler.
+try:
+    import neal
+except ImportError:
+    neal = None
 
-  Parameters:
-  linear (dict): Linear coefficients of the model.
-  quadratic (dict): Quadratic coefficients of the model.
-  private_token (str): API token for D-Wave.
-  runs (int): Number of reads for the sampler.
-
-  Returns:
-  dimod.SampleSet: Sample set returned by D-Wave sampler.
-  float: Connection time.
-  float: Embedding time.
-  float: Response time.
-  """
-  vartype = dimod.BINARY
-  bqm = dimod.BinaryQuadraticModel(linear, quadratic, 0.0, vartype)
-  start_time = time.time()
-  dwave_sampler = DWaveSampler(token = private_token, solver={'topology__type': 'pegasus'})
-  connection_time = time.time() - start_time
-
-  start_time = time.time()
-  sampler = EmbeddingComposite(dwave_sampler)
-  embedding_time = time.time() - start_time
-  start_time = time.time()
-  sample_set = sampler.sample(bqm, num_reads=runs)
-  response_time = time.time() - start_time
-  return sample_set, connection_time, embedding_time, response_time
 
 def get_linear_quadratic_dict(W):
     """Computes the QUBO matrix for the Minimum Cut problem given a weight matrix W."""
-    n = W.shape[0]  # Number of nodes
+    n = W.shape[0]
     linear = {}
     quadratic = {}
-    
+
     for i in range(n):
-        linear[i] = np.sum(W[i])  # Diagonal terms (degree of node)
+        linear[i] = np.sum(W[i])
         for j in range(n):
-            if i < j and W[i, j]!=0:
-                quadratic[(i, j)] = -W[i, j]  # Off-diagonal terms (negative adjacency)
-    return linear,quadratic
+            if i < j and W[i, j] != 0:
+                quadratic[(i, j)] = -W[i, j]
 
-def annealer_solver(G, private_token, n_samples=2000, **kwargs):
-  """
-  Solve the Maxcut problem on graph G using a D-Wave annealer.
+    return linear, quadratic
 
-  Parameters:
-  G (networkx.Graph): Graph for which Maxcut is to be solved.
-  private_token (str): API token for D-Wave.
-  n_samples (int): Number of samples to collect.
 
-  Returns:
-  pandas.DataFrame: Dataframe containing samples.
-  dict: Dictionary containing information about execution times.
-  """
-  start_time = time.time()
-  w = nx.adjacency_matrix(G).todense()
-  linear,quadratic = get_linear_quadratic_dict(w)
-  problem_formulation_time = time.time() - start_time
-  sample_set, connection_time, embedding_time, response_time = dwave_solver(linear, quadratic, private_token, runs=n_samples)
-  info_dict = sample_set.info['timing'].copy()
+def simulated_annealer_solver(G, n_samples=2000):
+    if neal is None:
+        raise ImportError(
+            "Missing package: dwave-neal. Install it with: pip install dwave-neal"
+        )
 
-  start_time = time.time()
-  samples_df = sample_set.to_pandas_dataframe()
-  sample_fetch_time = time.time() - start_time
+    start_time = time.time()
 
-  info_dict['problem_formulation_time'] = problem_formulation_time
-  info_dict['connection_time'] = connection_time
-  info_dict['embedding_time'] = embedding_time
-  info_dict['response_time'] = response_time
-  info_dict['sample_fetch_time'] = sample_fetch_time
-  return samples_df, info_dict
+    W = nx.adjacency_matrix(G).todense()
+    linear, quadratic = get_linear_quadratic_dict(W)
+
+    problem_formulation_time = time.time() - start_time
+
+    bqm = dimod.BinaryQuadraticModel(
+        linear,
+        quadratic,
+        0.0,
+        dimod.BINARY
+    )
+
+    start_time = time.time()
+
+    sampler = neal.SimulatedAnnealingSampler()
+    sample_set = sampler.sample(
+        bqm,
+        num_reads=n_samples
+    )
+
+    response_time = time.time() - start_time
+
+    start_time = time.time()
+    samples_df = sample_set.to_pandas_dataframe()
+    sample_fetch_time = time.time() - start_time
+
+    info_dict = {
+        "problem_formulation_time": problem_formulation_time,
+        "response_time": response_time,
+        "sample_fetch_time": sample_fetch_time
+    }
+
+    return samples_df, info_dict
